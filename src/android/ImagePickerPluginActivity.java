33f8c106d8cd20b5e1afa58d1e064371969de76a
James Kong <fishkingsin@gmail.com>
2017-09-19 15:04:28 +0800

    Update image picker

--- src/android/ImagePickerPluginActivity.java
+++ src/android/ImagePickerPluginActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.esafirm.imagepicker.model.Image;
 import com.kaopiz.kprogresshud.KProgressHUD;
 
 import java.util.ArrayList;
+
+import me.iwf.photopicker.PhotoPicker;
+import me.iwf.photopicker.PhotoPreview;
 
 public class ImagePickerPluginActivity extends Activity {
     private static final String TAG = ImagePickerPluginActivity.class.getSimpleName();
@@ -19,6 +21,7 @@ public class ImagePickerPluginActivity extends Activity {
     private int height;
     FakeR fakeR;
     private int maxImages;
+    private ArrayList<String> preselectedAsset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,74 +33,51 @@ public class ImagePickerPluginActivity extends Activity {
         this.width = bundle.getInt("WIDTH");
         this.height = bundle.getInt("HEIGHT");
         this.maxImages = bundle.getInt("MAX_IMAGES");
-        com.esafirm.imagepicker.features.ImagePicker
-                .create(this)
-                .returnAfterFirst(false)
-//                .folderMode(true) // folder mode (false by default)
-//                .folderTitle(getString(fakeR.getId("string","ALBUM"))) // folder selection title
-                .imageTitle("Tap to select") // image selection title
-                .multi() // multi mode (default mode)
-                .limit(maxImages) // max images can be selected (99 by default)
-                .showCamera(true) // show camera or not (true by default)
-//                .enableLog(false) // disabling log
-                .theme(fakeR.getId("style","ImagePickerTheme"))
-                .start(REQUEST_CODE_PICKER); // start image picker activity with request code
+        this.preselectedAsset = bundle.getStringArrayList("preselectedAsset");
 
-
-        /*imagePicker.setImagePickerCallback(new ImagePickerCallback() {
-            @Override
-            public void onImagesChosen(List<ChosenImage> images) {
-                //dismiss dialog
-                // Display images
-                kProgressHUD.dismiss();
-                ArrayList<String> imageList = new ArrayList<String>();
-                for (ChosenImage file : images) {
-                    //resolve content parsing
-                    if (file.getQueryUri().contains("com.google.android.apps.photos.contentprovider") || file.getQueryUri().contains("com.google.android.apps.docs.storage.legacy") ) {
-                        imageList.add(Uri.fromFile(new File(file.getOriginalPath())).toString());
-                    } else {
-                        imageList.add(file.getQueryUri());
-                    }
-//
-                }
-                Bundle conData = new Bundle();
-                conData.putStringArrayList(KEY_FILES, imageList);
-
-                Intent intent = new Intent();
-                intent.putExtras(conData);
-                setResult(RESULT_OK, intent);
-                finishActivity(REQUEST_IMAGEPICKER);
-                finish();
-
-            }
-
-            @Override
-            public void onError(String message) {
-                // Do error handling
-                Log.d(TAG, message);
-            }
-        });
-        if (this.width > 0 && this.height > 0) {
-            imagePicker.ensureMaxSize(this.width, this.height);
+        if(this.preselectedAsset == null){
+            preselectedAsset = new ArrayList<String>();
         }
-        imagePicker.allowMultiple(); // Default is false
-        imagePicker.shouldGenerateMetadata(true);
-        imagePicker.setCacheLocation(CacheLocation.INTERNAL_APP_DIR);//use internal cache directory
-        imagePicker.shouldGenerateThumbnails(false); // Default is true
-        imagePicker.pickImage();*/
+
+        PhotoPicker.builder()
+                .setPhotoCount(this.maxImages)
+                .setGridColumnCount(4)
+                .setShowCamera(true)
+                .setPreviewEnabled(false)
+                .setShowGif(false)
+                .setSelected(this.preselectedAsset)
+                .start(ImagePickerPluginActivity.this);
+//        com.esafirm.imagepicker.features.ImagePicker
+//                .create(this)
+//                .returnAfterFirst(false)
+////                .folderMode(true) // folder mode (false by default)
+////                .folderTitle(getString(fakeR.getId("string","ALBUM"))) // folder selection title
+//                .imageTitle("Tap to select") // image selection title
+//                .multi() // multi mode (default mode)
+//                .limit(maxImages) // max images can be selected (99 by default)
+//                .showCamera(true) // show camera or not (true by default)
+////                .enableLog(false) // disabling log
+//                .theme(fakeR.getId("style","ImagePickerTheme"))
+//                .start(REQUEST_CODE_PICKER); // start image picker activity with request code
+
 
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE_PICKER && resultCode == RESULT_OK && data != null) {
-            ArrayList<String> imageList = new ArrayList<String>();
-            ArrayList<Image> images = (ArrayList<Image>)  com.esafirm.imagepicker.features.ImagePicker.getImages(data);
-            for (int i = 0 ; i < images.size() ; i++){
-                imageList.add(images.get(i).getPath());
+        if (resultCode == RESULT_OK &&
+                (requestCode == PhotoPicker.REQUEST_CODE || requestCode == PhotoPreview.REQUEST_CODE)) {
+            ArrayList<String> photos = null;
+            if (data != null) {
+                photos = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
             }
+//            ArrayList<String> imageList = new ArrayList<String>();
+//            ArrayList<Image> images = (ArrayList<Image>)  com.esafirm.imagepicker.features.ImagePicker.getImages(data);
+//            for (int i = 0 ; i < images.size() ; i++){
+//                imageList.add(images.get(i).getPath());
+//            }
             Bundle conData = new Bundle();
-            conData.putStringArrayList(KEY_FILES, imageList);
+            conData.putStringArrayList (KEY_FILES, photos);
 
             Intent intent = new Intent();
             intent.putExtras(conData);
@@ -111,30 +91,6 @@ public class ImagePickerPluginActivity extends Activity {
             finishActivity(REQUEST_IMAGEPICKER);
             finish();
         }
-        /*if (resultCode == RESULT_OK && data != null) {
-            if (requestCode == Picker.PICK_IMAGE_DEVICE) {
-//                show dialog
-                kProgressHUD = KProgressHUD.create(ImagePickerPluginActivity.this)
-                        .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
 
-                        .setDetailsLabel("Downloading data")
-                        .setCancellable(false)
-                        .setAnimationSpeed(2)
-                        .setDimAmount(0.5f)
-                        .show();
-
-                imagePicker.submit(data);
-            } else {
-                Intent intent = new Intent();
-                setResult(RESULT_CANCELED, intent);
-                finishActivity(REQUEST_IMAGEPICKER);
-                finish();
-
-            }
-        } else {
-            Intent intent = new Intent();
-            setResult(RESULT_CANCELED, intent);
-            finish();
-        }*/
     }
 }
